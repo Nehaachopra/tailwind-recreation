b9c110c37c8e9556d0840eab5252dd444cf20aee
Neha Chopra <chopraneha211@gmail.com>
2026-03-20 08:43:36 +0530

    Add HTML page to test ChaiCSS

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
-// Version 1
-document.querySelectorAll("[class]").forEach(el => {
-  let styles = "";
-  el.classList.forEach(cls => {
-    if (cls.startsWith("chai-p-")) {
-      const value = cls.replace("chai-p-", "");
-      styles += `padding: ${value}px; `;
-    }
+const VERSION = 2; // change to 1 or 2
 
-    else if (cls.startsWith("chai-m-")) {
-      const value = cls.replace("chai-m-", "");
-      styles += `margin: ${value}px; `;
-    }
+if (VERSION === 1) {
+  // Version 1
+  document.querySelectorAll("[class]").forEach(el => {
+    let styles = "";
+    el.classList.forEach(cls => {
+      if (cls.startsWith("chai-p-")) {
+        const value = cls.replace("chai-p-", "");
+        styles += `padding: ${value}px; `;
+      }
+
+      else if (cls.startsWith("chai-m-")) {
+        const value = cls.replace("chai-m-", "");
+        styles += `margin: ${value}px; `;
+      }
+    });
+
+    el.setAttribute("style", styles);
+  })
+}
+
+else if (VERSION === 2) {
+  // Version 2
+  document.querySelectorAll("[class]").forEach(el => {
+    el.classList.forEach(cls => {
+
+      // Padding
+      if (cls.startsWith("chai-p-")) {
+        const key = cls.replace("chai-p-", "");
+        el.style.padding = `${key}px`;
+      }
+
+      // Margin
+      else if (cls.startsWith("chai-m-")) {
+        const key = cls.replace("chai-m-", "");
+        el.style.margin = `${key}px`;
+      }
+
+      // Background
+      else if (cls.startsWith("chai-bg-rgb-")) {
+        const [r, g, b] = cls.replace("chai-bg-rgb-", "").split("-");
+        el.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      }
+      else if (cls.startsWith("chai-bg-")) {
+        const key = cls.replace("chai-bg-", "");
+        el.style.backgroundColor = `${key}`;
+      }
+    });
   });
-
-  el.setAttribute("style", styles);
-})
-
-// Version 2
-document.querySelectorAll("[class]").forEach(el => {
-  el.classList.forEach(cls => {
-
-    // Padding
-    if (cls.startsWith("chai-p-")) {
-      const key = cls.replace("chai-p-", "");
-      el.style.padding = `${key}px`;
-    }
-
-    // Margin
-    if (cls.startsWith("chai-m-")) {
-      const key = cls.replace("chai-m-", "");
-      el.style.margin = `${key}px`;
-    }
-
-  });
-});
+}
