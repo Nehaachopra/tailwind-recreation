2b7f1dbc38825c0d6eedf27872723e2a5cdd8b2a
Neha Chopra <chopraneha211@gmail.com>
2026-03-20 07:57:52 +0530

    Add inline css based on class name using JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-// Display utilities
-const displayMap = {
-  "chai-d-block": "block",
-  "chai-d-inline": "inline",
-  "chai-d-inline-block": "inline-block",
-  "chai-d-flex": "flex",
-  "chai-d-grid": "grid",
-  "chai-d-none": "none",
-}
+// Version 1
+document.querySelectorAll("[class]").forEach(el => {
+  let styles = "";
+  el.classList.forEach(cls => {
+    if (cls.startsWith("chai-p-")) {
+      const value = cls.replace("chai-p-", "");
+      styles += `padding: ${value}px; `;
+    }
 
-Object.entries(displayMap).forEach(([className, value]) => {
-  document.querySelectorAll(`.${className}`).forEach(el => {
-    el.style.display = value;
+    else if (cls.startsWith("chai-m-")) {
+      const value = cls.replace("chai-m-", "");
+      styles += `margin: ${value}px; `;
+    }
+  });
+
+  el.setAttribute("style", styles);
+})
+
+// Version 2
+document.querySelectorAll("[class]").forEach(el => {
+  el.classList.forEach(cls => {
+
+    // Padding
+    if (cls.startsWith("chai-p-")) {
+      const key = cls.replace("chai-p-", "");
+      el.style.padding = `${key}px`;
+    }
+
+    // Margin
+    if (cls.startsWith("chai-m-")) {
+      const key = cls.replace("chai-m-", "");
+      el.style.margin = `${key}px`;
+    }
+
   });
 });
